9a0bc5ac6898fca351d6746b2cab71b902f2c75a
Bohdan <104573777+yashbon@users.noreply.github.com>
2023-10-18 13:00:17 +0300

    Initialize gh-pages

---
+++ static/js/56.70da42a0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[56],{2729:function(e,n,o){o.r(n),o.d(n,{default:function(){return i}});var t=o.p+"static/media/pngwing.3e29030bc5d75ce77cd7.png",c=o(184),i=function(){return console.log("Home Page"),(0,c.jsxs)(c.Fragment,{children:[(0,c.jsx)("img",{src:t,alt:"",style:{width:100,margin:"50px 0"}}),(0,c.jsx)("h2",{children:"Welcome to phonebook app!"})]})}}}]);
+//# sourceMappingURL=56.70da42a0.chunk.js.map
