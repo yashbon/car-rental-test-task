4e17b0ce950d9c8d5f7c2923744038415e08b3a2
Bohdan <yashbon@ukr.net>
2023-10-21 15:03:05 +0300

    Update Catalog.jsx

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,6 +1,10 @@
 import AdvertsList from 'components/Adverts/AdvertsList/AdvertsList';
+import { useDispatch } from 'react-redux';
+import { fetchAdverts } from 'redux/adverts/advertsOperations';
 
 const Catalog = () => {
+    const dispatch = useDispatch();
+    dispatch(fetchAdverts());
     return (
         <>
             <h2>catalog</h2>
