762615ec2f90f99836bb3b71ce4be5a9b7ea0f1c
yashbon <104573777+yashbon@users.noreply.github.com>
2023-10-22 22:20:11 +0000

    Deploying to gh-pages from @ yashbon/car-rental-test-task@59876151a97d9e1ee20bd128febac197481faf7b 🚀

---
+++ static/js/924.e9c54c16.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcar_rental_test_task=self.webpackChunkcar_rental_test_task||[]).push([[924],{7395:function(t,e,a){a.r(e);var r=a(620),s=a(9434),n=a(5247),c=a(184);e.default=function(){var t=(0,s.v9)(n.T).favorites;return(0,c.jsx)(c.Fragment,{children:(0,c.jsx)(r.Z,{adverts:t})})}}}]);
+//# sourceMappingURL=924.e9c54c16.chunk.js.map
